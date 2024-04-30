eb122ec359791e39a1c64832e63138a2ebf4acf3
Yury Buzinau <buzinov.yura@gmail.com>
2024-04-30 20:25:33 +0400

    HW_5 refactor: duplicate code in functions is wrapped in decorators
    Added module decorators_hw_5.py

--- home_work/hw_5/main_hw_5.py
+++ home_work/hw_5/main_hw_5.py
@@ -1,6 +1,7 @@
 """
 This module contain solution for HW#5
 """
+from home_work.hw_5.decorators_hw_5 import validate_string_type, validate_string_is_not_empty
 
 # - Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
 # - Напишите программу, которая добавляет ‘ing’ к словам
@@ -17,11 +18,10 @@ TWO_WORDS = 'Ivanou Ivan'
 TEXT_WITH_SPACES = "   String without trailing spaces   "
 
 
-def replace_hash_sign_on_slash_sign(*, text: str) -> str:
+@validate_string_type
+@validate_string_is_not_empty
+def replace_hash_sign_on_slash_sign(text: str) -> str:
     """ Replaces the '#' symbol with the '/' symbol in the given text. """
-
-    assert isinstance(text, str), "Argument 'string' must be a string."
-    assert text, "Length of text must be greater than 0"
 
     if "#" in text:
         return text.replace("#", "/")
@@ -29,30 +29,30 @@ def replace_hash_sign_on_slash_sign(*, text: str) -> str:
     return text
 
 
+@validate_string_type
+@validate_string_is_not_empty
 def add_ing_to_words(text: str) -> str:
     """Adds 'ing' to words"""
 
-    assert isinstance(text, str), "Argument 'string' must be a string."
-    assert text, "Length of text must be greater than 0"
     words = text.split(" ")
 
     return " ".join([f"{item}ing" for item in words])
 
 
-def swap_two_words(words: str) -> str:
+@validate_string_type
+def swap_two_words(text: str) -> str:
     """Swaps the positions of two words in a sentence."""
 
-    assert isinstance(words, str), "Argument 'string' must be a string."
-    words = words.split(" ")
+    words = text.strip().split(" ")
     assert len(words) == 2, "The input sentence must contain exactly two words."
 
     return f"{words[1]} {words[0]}"
 
 
+@validate_string_type
+@validate_string_is_not_empty
 def remove_trailing_spaces(text: str) -> str:
     """ Removes leading and trailing spaces from a string. """
-    assert isinstance(text, str), "Argument 'string' must be a string."
-    assert text, "Length of text must be greater than 0"
 
     return text.strip()
 
@@ -60,5 +60,5 @@ def remove_trailing_spaces(text: str) -> str:
 if __name__ == '__main__':
     print(replace_hash_sign_on_slash_sign(text=SOME_TEXT_1))
     print(add_ing_to_words(text=SOME_TEXT_2))
-    print(swap_two_words(words=TWO_WORDS))
+    print(swap_two_words(text=TWO_WORDS))
     print(remove_trailing_spaces(text=TEXT_WITH_SPACES))
