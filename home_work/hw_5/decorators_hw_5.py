46991423e6f65473bbffd87d2b95e49813fd5f41
Yury Buzinau <buzinov.yura@gmail.com>
2024-04-30 20:39:31 +0400

    HW_5 style: add docstring in decorators
    Changes were made in decorators_hw_5.py

--- home_work/hw_5/decorators_hw_5.py
+++ home_work/hw_5/decorators_hw_5.py
@@ -4,6 +4,9 @@ This module contains decorators for validating string params
 
 
 def validate_string_type(func):
+    """Decorator to ensure that
+    the argument passed to the decorated function is of type string.
+    """
     def wrapper(text):
         assert isinstance(text, str), f"Argument in function '{func.__name__}' must be a string."
         return func(text)
@@ -12,6 +15,9 @@ def validate_string_type(func):
 
 
 def validate_string_is_not_empty(func):
+    """Decorator to ensure that
+    the argument passed to the decorated function is not an empty string.
+    """
     def wrapper(text):
         assert text.strip(), "Length of text must be greater than 0"
         return func(text)
