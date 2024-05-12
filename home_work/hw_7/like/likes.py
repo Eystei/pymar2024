1a9088960e35dfd810ab57619256dcb4bd74a9cc
buzinov.yu <buzinov.yura@gmail.com>
2024-05-12 10:45:48 +0400

    HW_7 refactor: likes.py replace if-elif-else with match-case

--- home_work/hw_7/like/likes.py
+++ home_work/hw_7/like/likes.py
@@ -9,18 +9,14 @@ class Likes:
         if not all(isinstance(name, str) and name for name in names):
             raise TypeError("Names must be strings and not empty string.")
 
-        num_likes = len(names)
-
-        if num_likes == 0:
-            return "no one likes this"
-
-        if num_likes == 1:
-            return f"{names[0]} likes this"
-
-        if num_likes == 2:
-            return f"{names[0]} and {names[1]} like this"
-
-        if num_likes == 3:
-            return f"{names[0]}, {names[1]} and {names[2]} like this"
-
-        return f"{names[0]}, {names[1]} and {num_likes - 2} others like this"
+        match a := len(names):
+            case 0:
+                return "no one likes this"
+            case 1:
+                return f"{names[0]} likes this"
+            case 2:
+                return f"{names[0]} and {names[1]} like this"
+            case 3:
+                return f"{names[0]}, {names[1]} and {names[2]} like this"
+            case _:
+                return f"{names[0]}, {names[1]} and {a - 2} others like this"
