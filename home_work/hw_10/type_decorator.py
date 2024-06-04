f2a454cd96be759c8c1f53c6c57ae9b7baf8b9cf
buzinov.yu <buzinov.yura@gmail.com>
2024-06-04 09:05:03 +0400

    HW_10 feat: add second solution in type_decorator.py
    Homework10: Yura Buzinau

--- home_work/hw_10/type_decorator.py
+++ home_work/hw_10/type_decorator.py
@@ -1,4 +1,5 @@
 """Module for type-based decorator."""
+import loguru
 
 
 def typed(type_):
@@ -9,8 +10,9 @@ def typed(type_):
 
         def wrapper(*args, **kwargs):
             """Wrapper function to enforce argument types."""
-            args = [type_(arg) for arg in args]
-            kwargs = {k: type_(v) for k, v in kwargs.items()}
+            args = [arg if isinstance(arg, type_) else type_(arg) for arg in args]
+            kwargs = {k: v if isinstance(v, type_) else type_(v) for k, v in kwargs.items()}
+
             return func(*args, **kwargs)
 
         return wrapper
@@ -19,21 +21,21 @@ def typed(type_):
 
 
 @typed(type_=str)
-def add_str(a, b):
-    """Concatenate two strings."""
-    return a + b
+def add_str(*args, **kwargs):
+    """Concatenate multiple strings."""
+    return ''.join(args) + ''.join(kwargs.values())
 
 
 @typed(type_=int)
-def add_int(a, b, c):
-    """Add three integers."""
-    return a + b + c
+def add_int(*args, **kwargs):
+    """Add integers."""
+    return sum(args) + sum(kwargs.values())
 
 
 @typed(type_=float)
-def add_float(a, b, c):
-    """Add three floating point numbers."""
-    return a + b + c
+def add_float(*args, **kwargs):
+    """Add floating point numbers."""
+    return sum(args) + sum(kwargs.values())
 
 
 assert add_str("3", 5) == '35', \
@@ -45,8 +47,46 @@ assert add_str(5, 5) == '55', \
 assert add_str('a', 'b') == 'ab', \
     f"Expected 'ab', got {add_str('a', 'b')}"
 
-assert add_int(5, 6, 7) == 18, \
+assert add_int(5, "6", 7) == 18, \
     f"Expected 18, got {add_int(5, 6, 7)}"
 
 assert add_float(0.1, 0.2, 0.4) == 0.7000000000000001, \
     f"Expected 0.7000000000000001, got {add_float(0.1, 0.2, 0.4)}"
+
+
+##########################################################################
+# For Sergey Stamal
+# Why we should use decorator for typing if solution below seems more
+# flexible. Without uses any decorators.
+
+def add(type_, *args, **kwargs):
+    """   Converts all positional and keyword arguments to the specified type,
+    then concatenates them or sums.
+    """
+
+    args = [arg if isinstance(arg, type_) else type_(arg) for arg in args]
+    kwargs = {k: v if isinstance(v, type_) else type_(v) for k, v in kwargs.items()}
+
+    if type_ == str:
+        return ''.join(args) + ''.join(kwargs.values())
+    elif type_ in [int, float]:
+        return sum(args) + sum(kwargs.values())
+    else:
+        loguru.logger.error("Function support only str | float | int")
+        raise TypeError("Function support only str | float | int")
+
+
+assert add(str, "3", 5) == '35', \
+    f"Expected '35', got {add('3', 5)}"
+
+assert add(str, 5, 5) == '55', \
+    f"Expected '55', got {add(5, 5)}"
+
+assert add(str, 'a', 'b') == 'ab', \
+    f"Expected 'ab', got {add('a', 'b')}"
+
+assert add(int, 5, "6", 7) == 18, \
+    f"Expected 18, got {add(5, 6, 7)}"
+
+assert add(float, 0.1, 0.2, 0.4) == 0.7000000000000001, \
+    f"Expected 0.7000000000000001, got {add(0.1, 0.2, 0.4)}"
