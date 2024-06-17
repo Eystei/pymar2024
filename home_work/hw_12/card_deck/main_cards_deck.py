e797e249a27bb56b800d510d2d3370626a44cca4
buzinov.yu <buzinov.yura@gmail.com>
2024-06-17 09:48:05 +0400

    HW_12 feat: solve problems from homework_12 (1/2)
    Additional info:
    - task completed: cards_deck
    
    Homework12: Yura Buzinau

---
+++ home_work/hw_12/card_deck/main_cards_deck.py
@@ -0,0 +1,27 @@
+from cards_deck import CardsDeck
+
+
+def main():
+    deck = CardsDeck()
+
+    deck.shuffle()
+
+    deck.show_remaining_cards()
+
+    deck.get_card()
+    deck.get_card()
+    deck.get_card()
+
+    deck.show_remaining_cards()
+
+    deck.shuffle()
+
+    deck.show_remaining_cards()
+
+    deck.get_card()
+
+    deck.show_remaining_cards()
+
+
+if __name__ == "__main__":
+    main()
