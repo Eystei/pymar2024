afc08597cc8da0b39b56f9b293b0a7a94d8f849b
buzinov.yu <buzinov.yura@gmail.com>
2024-06-23 09:37:50 +0400

    HW_14 feat: add simple validator before eval() in calculator.py
    Homework14: Yura Buzinau

--- home_work/hw_14/calculator/calculator.py
+++ home_work/hw_14/calculator/calculator.py
@@ -1,27 +1,30 @@
 from loguru import logger
 
+ALLOWED_CHARS = '0123456789+-*/()%'
+
+
+def simple_validator(expression):
+    for char in expression:
+        if char not in ALLOWED_CHARS:
+            return False, char
+    return True, None
+
 
 def evaluate_expression(expression):
+    expression = expression.replace(' ', '')
+    valid, invalid_char = simple_validator(expression)
+    if not valid:
+        logger.error(f"Invalid char --> {invalid_char}")
+        return f"Invalid char --> {invalid_char}"
+
     try:
-        expression = expression.replace("\\", "/")
-
         return eval(expression)
-
     except ZeroDivisionError:
         logger.error("Division by zero.")
         return "Division by zero."
-
-    except ValueError as e:
-        logger.error(f"Value error: {e}")
-        return str(e)
-
     except SyntaxError:
         logger.error("Syntax error in the expression.")
         return "Syntax error in the expression."
-
-    except NameError:
-        logger.error("Name error in the expression.")
-        return "Name error in the expression."
 
 
 def run():
@@ -50,6 +53,6 @@ if __name__ == "__main__":
     assert evaluate_expression("(2+3)*4") == 20
     assert evaluate_expression("1/0") == "Division by zero."
     assert evaluate_expression("2+*3") == "Syntax error in the expression."
-    assert evaluate_expression("2 + abc") == "Name error in the expression."
+    assert evaluate_expression("2 + abc") == "Invalid char --> a"
 
     logger.success("All tests passed.")
